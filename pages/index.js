da78cdbfc7c0d3157171df1623325fb2ce4917c2
Adrian Dinis <adrian@marzeelabs.org>
2021-04-21 11:34:22 +0100

    Add example image

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import Head from 'next/head'
+import Image from 'next/image'
+
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -18,6 +20,13 @@ export default function Home() {
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
+
+        <Image
+          src="/example.jpg"
+          alt="Example image"
+          width={500}
+          height={500}
+        />
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
